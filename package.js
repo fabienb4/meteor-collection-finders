0a2b85cff9b128a67873a359e8853f1dd9e67fce
FabienB4 <fabienb4.b4soft@gmail.com>
2015-04-05 12:27:49 -0600

    0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'fabienb4:collection-finders',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'A simple package to allow you to easily find documents within your collections using `id` or `name`.',
   // URL to the Git repository containing the source code for this package.
